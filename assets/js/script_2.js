b69ba8172e2c4d2bc2717963aa0088645709524c
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2023-07-31 21:16:08 -0300

    Create Portfolio

---
+++ assets/js/script_2.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.chart').easyPieChart({
+      size: 150,
+      barColor: "#6A5ACD",
+      scaleLength: 0,
+      lineWidth: 15,
+      trackColor: "#666565 ",
+      lineCap: "circle",
+      animate: 3000,
+      
+    });
+  });
+  
