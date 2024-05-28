2fbc53ea0d1e8c9fddb8d177c7dbf4ca199a2a62
José Renato Felix da Silva <93296913+joserenatofelix@users.noreply.github.com>
2024-05-27 21:13:41 -0300

    Create portfolio and menu
    Criação da estrutura do react.js e alguns componentes e menu.

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+import { Link } from "lucide-react";
+import { Button } from "./ui/button";
+
+//components
+import Nav from "./Nav";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* logo */}
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Renato<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+                
+                {/* desktop nov & hire me button */}
+                <div className="hidden xl:flex items-center gap-8">
+            <Nav />
+            <Link href="/contact">
+                <Button>Hire Me</Button>
+            </Link>
+        </div>
+
+        {/*mobile nav*/}
+        <div className="xl:hidden">mobile nav</div>
+        
+        </div>
+     </header>
+    );
+};
+
+export default Header;
